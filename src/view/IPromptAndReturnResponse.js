2b525d320b6779dee090b629031d5cb57e264ef0
pcs-anthonyjohnson <aj72325@gmail.com>
2022-07-15 14:01:24 -0600

    Started User Input Encapsulation
    Pertaining to #1, this commit starts on the encapsulation of user
    input logic by creating an interface that defines a prompt method
    for implementers to code. Then, PromptAndReturnSimpleStrategy was
    then created and tested to print out a message and return a
    string response.

---
+++ src/view/IPromptAndReturnResponse.js
@@ -0,0 +1,10 @@
+
+let IPromptAndReturnResponse = class 
+{
+  prompt()
+  {
+    console.log('Implementer of this interface did not implement this method.');
+  }
+}
+
+module.exports = IPromptAndReturnResponse;
